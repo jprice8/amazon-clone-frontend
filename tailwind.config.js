1ed0354d75816c55aacb0191423b2baf547b590b
Jackson Price <59149601+jprice8@users.noreply.github.com>
2021-11-29 17:13:58 -0600

    First commit.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./shared/components/*.{js,ts,jsx,tsx}",
+    "./shared/utils/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ember: ["Amazon Ember", "sans-serif"],
+      },
+      colors: {
+        amznPriNav: {
+          DEFAULT: "#131921",
+        },
+        amznSecNav: {
+          DEFAULT: "#232F3E",
+        },
+        amznYellow: {
+          DEFAULT: "#FEBD69",
+        },
+        amznGray: {
+          DEFAULT: "#D5DBDB",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
